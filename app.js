6f984ba6ddc7a34f6c3b4647201117a86c2023cc
Desh Deepak Singh <Desh.Deepak.Singh@ibm.com>
2023-12-18 17:49:22 +0530

    adding files and js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const changeColour = () => {
-    $('body').css('background-color','red');
+    let x = Math.floor((Math.random() * 4) + 1);
+    let side = $(`#g${x}`).text();
+    console.log($(`#g${x}`).text()); 
+    $(`#s${x}`).text(side);
 }
